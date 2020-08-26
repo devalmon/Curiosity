e30d7dcca7a2480f5e36bf8d75949be9ca3efa4f
Alexey B <phlood@yandex.ru>
2020-08-26 21:47:58 +0300

    Initial Commit

---
+++ Curiosity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Curiosity
+//
+//  Created by Alexey Baryshnikov on 26.08.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Curiosity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Curiosity
+//
+//  Created by Alexey Baryshnikov on 26.08.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
