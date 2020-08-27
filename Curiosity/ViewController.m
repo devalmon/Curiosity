38fd6b16a170e83b5f54737351ab37c445f85a06
Alexey B <phlood@yandex.ru>
2020-08-27 14:10:28 +0300

    roll out

--- Curiosity/ViewController.m
+++ Curiosity/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ABMarsData.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"Sensors data";
+#pragma mark Background
+    self.view.backgroundColor = UIColor.whiteColor;
+    UIImageView *backgroundImage = [[UIImageView alloc] init];
+    
+    backgroundImage.image = [UIImage imageNamed:@"mars.jpg"];
+    backgroundImage.alpha = 0.75f;
+    [self.view addSubview:backgroundImage];
+    backgroundImage.translatesAutoresizingMaskIntoConstraints = NO;
+    [backgroundImage.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:0].active = YES;
+    [backgroundImage.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:0].active = YES;
+    [backgroundImage.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:0].active = YES;
+    [backgroundImage.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:0].active = YES;
+    
+#pragma mark Text representation
+    UITextView *textSection = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    textSection.backgroundColor = UIColor.clearColor;
+    textSection.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    
+#pragma mark URLSession
+    NSString *baseURLString = @"https://api.maas2.apollorion.com/";
+    NSURL *baseURL = [NSURL URLWithString:baseURLString];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:baseURL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error != nil) {
+            NSLog(@"%@", error);
+        }
+            
+        ABMarsData *jsonData = [ABMarsData fromData:data error:&error];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            textSection.text = [NSString stringWithFormat:@"Curiosity rover data\n\nmax temp: %ld °C\nmin temp: %ld °C\nsol: %ld\nseason: %@\natmospher opacity: %@\npressure: %@ %ld kPa\nUV index: %@", [jsonData maxTemp], [jsonData minTemp], [jsonData sol], [jsonData season], [jsonData atmoOpacity], [jsonData pressureString], [jsonData pressure], [jsonData localUvIrradianceIndex]];
+            [textSection setFont:[UIFont fontWithName:@"Copperplate" size:22]];
+        });
+        
+    }];
+    [dataTask resume];
+#pragma mark mainStackView
+    UIStackView *mainStackView = [[UIStackView alloc] init];
+    mainStackView.axis = UIAxisVertical;
+    mainStackView.distribution = UIStackViewDistributionFillProportionally;
+    mainStackView.spacing = 20.f;
+    
+    [mainStackView addSubview:textSection];
+    [self.view addSubview:mainStackView];
+    mainStackView.translatesAutoresizingMaskIntoConstraints = NO;
+    [mainStackView.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20].active = YES;
+    [mainStackView.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:20].active = YES;
+    [mainStackView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20].active = YES;
+    [mainStackView.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:20].active = YES;
+    
+    [textSection.leadingAnchor constraintEqualToAnchor:mainStackView.leadingAnchor constant:20].active = YES;
+    [textSection.topAnchor constraintEqualToAnchor:mainStackView.topAnchor constant:20].active = YES;
+    [textSection.bottomAnchor constraintEqualToAnchor:mainStackView.bottomAnchor constant:-20].active = YES;
+    [textSection.trailingAnchor constraintEqualToAnchor:mainStackView.trailingAnchor constant:-20].active = YES;
 }
 
 
